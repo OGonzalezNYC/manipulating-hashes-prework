15c8e3f2958d39ed22e3bd3cb453b2b0e4a813bc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-12 23:01:36 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,8 @@ def first_challenge
    data.each do |attribute, value|
      value.delete_if do |flavor|
        flavor = "strawberry"
+     end 
+   end 
   #remember to return your newly altered contacts hash!
   contacts
 end
